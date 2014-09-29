dd18ad33e47eaf71ca249a6b20e54632a75f1c04
zws3mb <zws3mb@virginia.edu>
2014-09-29 14:50:42 -0400

    herpderp for master

---
+++ herpderp.py
@@ -0,0 +1,9 @@
+def herpmaderp(msg):
+	temp=""
+	for let in msg:
+		if let=='a' or let=='e' or let=='i' or let=='u' or let=='o':
+			let='erp'
+		temp+=let
+	print temp
+if __name__=='__main__':
+	ermagerd('hello')
